b4104803eea1e38bbb194fe902d4c242632fdd64
abraham241 <huguesmboundou@gmail.com>
2024-05-03 14:47:39 +0100

    Version 1 page JS

---
+++ index.js
@@ -0,0 +1,19 @@
+const info = document.querySelector(".infos")
+
+const allBtns = document.querySelectorAll('input[type="button"]')
+
+function val(resultat){
+    Form.fenetre.value = Form.fenetre.value + resultat
+}
+function calcule(){
+    if(Form.fenetre.value == ""){
+        info.innerText =`Veillez saisir des chiffres`;
+        allBtns.forEach(btn =>{
+            btn.addEventListener('click', function(){
+                info.innerText =""
+            })
+        })
+    }else{
+        Form.fenetre.value = eval(Form.fenetre.value)
+    }
+}
